57cbccff2fea54af164194653631a0cc68b6c4f2
pablitodon <pavelkazakevich040196@gmail.com>
2024-09-14 23:35:52 +0300

    add search input and usedebounce

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -1,37 +1,42 @@
 /* eslint-disable no-unused-vars */
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL=import.meta.env.VITE_NEWS_BASE_API_URL
-const API_KEY=import.meta.env.VITE_NEWS_API_KEY
+const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL;
+const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
-export const getNews = async({page_number=1,page_size=10,category,keywords}) => {
-try {
-    const response =  await axios.get(`${BASE_URL}search`,{
-        params:{
-            apiKey:API_KEY,
-            page_number,
-            page_size,
-            category,
-            keywords
-        }
-    })
-      return response.data
-} catch (error) {
+export const getNews = async ({
+  page_number = 1,
+  page_size = 10,
+  category,
+  keywords,
+}) => {
+  try {
+    const response = await axios.get(`${BASE_URL}search`, {
+      params: {
+        apiKey: API_KEY,
+        page_number,
+        page_size,
+        category,
+        keywords,
+      },
+    });
+    return response.data;
+  } catch (error) {
     console.log(error);
-}
-}
+  }
+};
 
-export const getСategories  = async() => {
-try {
-    const response =  await axios.get(`${BASE_URL}available/categories`,{
-        params:{
-            apiKey:API_KEY,
-        }
-    })
+export const getСategories = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}available/categories`, {
+      params: {
+        apiKey: API_KEY,
+      },
+    });
     console.log(response.data);
-    
-      return response.data
-} catch (error) {
+
+    return response.data;
+  } catch (error) {
     console.log(error);
-}
-}
+  }
+};
